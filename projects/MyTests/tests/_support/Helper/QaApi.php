fe21c12d0114158c2f3576cc5bf19da73352039c
Oleg <oleg@aphixsoftware.com>
2016-10-22 21:42:32 +0100

    example of repository with tests

---
+++ projects/MyTests/tests/_support/Helper/QaApi.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MyTests\Helper;
+
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+use MyTests\_generated\QaApi\Module;
+
+class QaApi extends Module
+{
+
+}
---
+++ projects/MyTests/tests/functional/FirstTestCest.php
@@ -0,0 +1,19 @@
+<?php
+namespace MyTests;
+use MyTests\FunctionalTester;
+
+class FirstTestCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
